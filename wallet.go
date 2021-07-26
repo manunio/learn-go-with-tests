96a77141749c4e1264093524a4d39a2df3f5fc8e
manunio <max.manu.nair@gmail.com>
2021-07-26 10:46:37 +0530

    tdd: Pointers & errors

---
+++ wallet.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return (*w).balance
+}
+
+var ErrsInSufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrsInSufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
