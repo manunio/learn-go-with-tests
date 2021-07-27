d4f44f75e617881af5948566b78430ea17279711
manunio <max.manu.nair@gmail.com>
2021-07-27 19:25:29 +0530

    tdd: select [2]

--- select/racer.go
+++ select/racer.go
@@ -1,23 +1,33 @@
 package _select
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) (winner string) {
-	aDuration := measureResponse(a)
-	bDuration := measureResponse(b)
+const tenSecondTimeout = 10 * time.Second
 
-	if aDuration < bDuration {
-		return a
-	}
-
-	return b
+func Racer(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
 }
 
-func measureResponse(url string) time.Duration {
-	start := time.Now()
-	_, _ = http.Get(url)
-	return time.Since(start)
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		_, _ = http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
