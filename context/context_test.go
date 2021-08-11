180f3ac8897d623151cdf7ce759750f0bae20134
manunio <max.manu.nair@gmail.com>
2021-08-11 16:03:28 +0530

    tdd: context [3]

--- context/context_test.go
+++ context/context_test.go
@@ -2,6 +2,7 @@ package _context
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,37 +10,80 @@ import (
 )
 
 type SpyStore struct {
-	respone   string
-	cancelled bool
-	t         *testing.T
+	respone string
+	t       *testing.T
 }
 
-func (s *SpyStore) assertWasCancelled() {
-	s.t.Helper()
-	if !s.cancelled {
-		s.t.Error("store was not told to cancel")
+// We are simulating a slow process where we build the result slowly by appending the string, character by
+// character in a goroutine. When the goroutine finishes its work it writes the string to the data channel.
+// The goroutine listens for the ctx.Done and will stop the work if a signal is sent in that channel.
+// Finally the code uses another select to wait for that goroutine to finish,
+// its work or for the cancellation to occur.
+
+func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
+	data := make(chan string, 1)
+
+	go func() {
+		var result string
+		for _, c := range s.respone {
+			select {
+			case <-ctx.Done():
+				s.t.Log("spy store got cancelled")
+				return
+			default:
+				time.Sleep(10 * time.Microsecond)
+				result += string(c)
+			}
+		}
+		data <- result
+	}()
+
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case res := <-data:
+		return res, nil
 	}
+
 }
 
-func (s *SpyStore) assertWasNotCancelled() {
-	s.t.Helper()
-	if s.cancelled {
-		s.t.Error("store was told to cancel")
-	}
+type SpyResponseWriter struct {
+	written bool
 }
 
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.respone
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
 }
 
-func (s *SpyStore) Cancel() {
-	s.cancelled = true
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
 }
 
 func TestServer(t *testing.T) {
+	const data = "hello, world"
+
+	t.Run("returns data from the store", func(t *testing.T) {
+		store := &SpyStore{respone: data, t: t}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if response.Body.String() != data {
+			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+		}
+
+	})
+
 	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
-		data := "hello, world"
 		store := &SpyStore{respone: data, t: t}
 		svr := Server(store)
 
@@ -49,29 +93,13 @@ func TestServer(t *testing.T) {
 		time.AfterFunc(5*time.Millisecond, cancel)
 		request = request.WithContext(cancellingCtx)
 
-		response := httptest.NewRecorder()
+		response := &SpyResponseWriter{}
 
 		svr.ServeHTTP(response, request)
 
-		store.assertWasCancelled()
-
-	})
-
-	t.Run("returns data from the store", func(t *testing.T) {
-		data := "hello, world"
-		store := &SpyStore{respone: data, t:t}
-		svr := Server(store)
-
-		request := httptest.NewRequest(http.MethodGet, "/", nil)
-		response := httptest.NewRecorder()
-
-		svr.ServeHTTP(response, request)
-
-		if response.Body.String() != data {
-			t.Errorf("got %s, want %s", response.Body.String(), data)
+		if response.written {
+			t.Error("a response should not have been written")
 		}
-
-		store.assertWasNotCancelled()
 
 	})
 
